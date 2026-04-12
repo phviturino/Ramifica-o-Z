bc3e8ab0bb9d5b509cab1cb154af63df0cb2f74c
Pedro Henrique <seuemail@gmail.com>
2026-04-12 13:19:55 -0300

    update

--- js/script.js
+++ js/script.js
@@ -17,20 +17,28 @@ botaoTema.addEventListener('click', () => {
     }
 });
 window.onload = function() {
-    // entrada de dados
-    let nome = prompt("Qual o seu nome, soldado?")
-    let idade = prompt("Bem-vindo ao recrutamento soldado " + nome + "! Qual a sua idade? ");
-
-    // h2 html
-    const elementosaudacao = document.getElementById('mensagem-saudacao');
-
-    if (nome) {
-        elementoSaudacao.textContent ="Boa sorte na missão, soldado" + nome + "!";
-    }
-
-    if (idade  >= 12) {
-        alert("Ótimo, você será enviado a Capital da Realidade Z!");
-    } else {
-        alert("Soldado, você será enviado para a zona sul da Realidade Z");
+    const elementoSaudacao = document.getElementById('mensagem-saudacao');
+    let nomeSalvo = localStorage.getItem('nomeSoldado');
+    if (elementoSaudacao && !nomeSalvo) {
+        let nome = prompt("Qual o seu nome, soldado?");
+        
+        if (nome) {
+            localStorage.setItem('nomeSoldado',nome);
+            elementoSaudacao.textContent = "Boa sorte na missão, soldado " + nome + "!";
+    
+            let idade = prompt("Bem-vindo ao recrutamento soldado " + nome + "! Qual a sua idade?");
+            
+            if (idade >= 12) {
+                alert("Ótimo, você será enviado a Capital da Realidade Z!");
+            } else {
+                alert("Soldado, você será enviado para a zona sul da Realidade Z");
+            }
+        } else if (elementoSaudacao && nomeSalvo) {
+            elementoSaudacao.textContent = "Bem-vindo de volta soldado " + nomeSalvo + "!";
+        }
+        const anoAtual = new Date().getFullYear();
+        if (anoAtual === 2026) {
+            alert("Ano de Grande Lançamento: Realidade Z está On!");
+        }
     }
 };
